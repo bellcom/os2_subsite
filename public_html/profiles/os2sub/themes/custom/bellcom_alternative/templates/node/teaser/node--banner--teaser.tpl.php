bf64ef295ccfd9948ebebae6778de6d5c18bd6c5
Franz Skaaning <fs@bellcom.dk>
2017-08-31 12:39:36 +0200

    Banner width

--- public_html/profiles/os2sub/themes/custom/bellcom_alternative/templates/node/teaser/node--banner--teaser.tpl.php
+++ public_html/profiles/os2sub/themes/custom/bellcom_alternative/templates/node/teaser/node--banner--teaser.tpl.php
@@ -1,4 +1,4 @@
-
+banner
 <?php 
 	$style='os2sub_banner';
 	$path=$node->field_banner_billede['und']['0']['uri'];
@@ -6,24 +6,15 @@
 	
 	<article id="node-<?php print $node->nid; ?>" class="banner <?php print $classes . " all"; ?> clearfix"<?php print $attributes; ?> 
 		style="background: url(<?php print file_create_url($style_url); ?>) no-repeat scroll ;height:400px;background-size: cover;">
-	
-	<div class="row">
-		<div class="col-md-6 ">
-	<div class="banner-wrapper color-<?php print $node->field_hvid_tekst['und']['0']['value']; ?>">
-		<div class="banner-headline"><?php print render($content['field_banner_headline']); ?></div>
-		<div class="banner-text"><?php print render($content['field_banner_text']); ?></div>
-		<?php print render($content['field_knap_link']);?>
-		
-		</div>
-	</div>
-	</div>
-	
-
-  
-  
-  
-  
-  
-</article>
-
-
+    <div class="container" role="banner">
+    	<div class="row">
+    		<div class="col-md-6 ">
+        	<div class="banner-wrapper color-<?php print $node->field_hvid_tekst['und']['0']['value']; ?>">
+        		<div class="banner-headline"><?php print render($content['field_banner_headline']); ?></div>
+        		<div class="banner-text"><?php print render($content['field_banner_text']); ?></div>
+        		<?php print render($content['field_knap_link']);?>    		
+      		</div>
+      	</div>
+    	</div>
+  	</div>	
+  </article>
