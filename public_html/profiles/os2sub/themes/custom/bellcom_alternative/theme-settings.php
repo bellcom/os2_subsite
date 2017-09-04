c8120528d9234ed1d958838cb923ddb9786e9f71
Stanislav Kutasevits <stan@bellcom.dk>
2017-09-04 14:04:44 +0300

    enabling theme settings

--- public_html/profiles/os2sub/themes/custom/bellcom_alternative/theme-settings.php
+++ public_html/profiles/os2sub/themes/custom/bellcom_alternative/theme-settings.php
@@ -4,7 +4,7 @@ include( dirname(__FILE__) . '/include/settings.inc');
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function bellcom_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
+function bellcom_alternative_form_system_theme_settings_alter(&$form, $form_state, $form_id = NULL) {
   if (isset($form_id)) {
     return;
   }
