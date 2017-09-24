1c2232ce3d3afdcc2d2fa9624a6359a2f782b256
Franz Skaaning <fs@bellcom.dk>
2017-09-24 18:11:04 +0200

    Afsnit

--- public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/paragraphs/paragraphs-item--afsnit-billede.tpl.php
+++ public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/paragraphs/paragraphs-item--afsnit-billede.tpl.php
@@ -27,17 +27,12 @@
  */
 ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <div class="paragraph-content"<?php print $content_attributes; ?>>  
-    <h2 class="js-expandmore">
-      <div class="toggle-wrap">
-        <div class="accordion__toggle">
-          <span></span>
-        </div>
-      </div>
+  <?php print render($content['field_image']); ?>
+  <div class="content"<?php print $content_attributes; ?>>  
+    <h2>
       <?php print render($content['field_paragraph_header']); ?>
     </h2>    
-    <div class="js-to_expand">
-      <?php print render($content['field_image']); ?>
+    <div>
       <?php print render($content['field_paragraph_text']); ?>
     </div>  
   </div>
