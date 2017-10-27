cb5ee014a4ca4629ecec052ac375a5150d2ea6de
franzskaaning <fs@bellcom.dk>
2017-10-27 07:50:47 +0200

    Selfvservice links, normal btn

--- public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/node/node--os2web-base-selfservice-link--spotbox.tpl.php
+++ public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/node/node--os2web-base-selfservice-link--spotbox.tpl.php
@@ -1,7 +1,7 @@
 <?php print render($title_prefix); ?>
 <?php print render($title_suffix); ?>
 <span role="listitem" id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> subsite-os2web_base_selfservice_link"<?php print $attributes; ?>>
-  <a class="btn btn-primary btn-lg btn-block" title="<?php print $field_spot_link['und'][0]['title']; ?> <?php print $field_os2web_base_field_source['und'][0]['value']; ?>" href="<?php print $field_spot_link['und'][0]['url']; ?>">
+  <a class="btn btn-primary btn-block" title="<?php print $field_spot_link['und'][0]['title']; ?> <?php print $field_os2web_base_field_source['und'][0]['value']; ?>" href="<?php print $field_spot_link['und'][0]['url']; ?>">
       <?php print $field_vist_navn[0]['value']; ?>
   </a>
 </span>
