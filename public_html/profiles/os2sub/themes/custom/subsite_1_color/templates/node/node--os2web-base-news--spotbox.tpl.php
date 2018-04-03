d2f4a73387c3c4f0f780c39ca8ff2f70cacd3da0
Morten Nissen <mail@hjemmesidekongen.dk>
2018-04-03 15:56:24 +0200

    BKDK-196

--- public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/node/node--os2web-base-news--spotbox.tpl.php
+++ public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/node/node--os2web-base-news--spotbox.tpl.php
@@ -1,22 +1,30 @@
-<article role="listitem" id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> subsite-spotbox subsite-box subsite-box-small-spacing"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-  <a class="news-spot-link" href="<?php print $node_url; ?>">
-    <span class="h3 aktuelt static">Aktuelt</span>
-  <span class="h4 subsite-spot-heading-title"><?php print $title; ?></span>
-  <?php if (isset($content['body'])): ?>
-    <span class="subsite-teaser-body-content">
-      <?php print render($content['body']); ?>
-    </span>
-  <?php endif; ?>
+<article role="listitem" id="node-<?php print $node->nid; ?>"
+         class="<?php print $classes; ?> subsite-spotbox subsite-box subsite-box-small-spacing"<?php print $attributes; ?>>
 
-   <span class="date-in-parts">
-       <span class="day"><?php  echo date("j", $node->created); ?>. </span>
-       <span class="month"><?php echo date("F", $node->created); ?></span>
-       <span class="year"><?php echo date("Y", $node->created); ?></span>
-    </span>
+    <?php if ( isset( $content['field_os2web_base_field_lead_img'] ) ): ?>
+        <!-- Begin - image -->
+        <div class="node__image">
+            <?php print render( $content['field_os2web_base_field_lead_img'] ); ?>
+        </div>
+        <!-- End - image -->
+    <?php endif; ?>
 
-  </a>
-  <?php print render($title_suffix); ?>
+    <a class="news-spot-link" href="<?php print $node_url; ?>">
+        <div class="date-in-parts">
+            <span class="day"><?php echo date("j", $node->created); ?>. </span>
+            <span class="month"><?php echo date("F", $node->created); ?></span>
+            <span class="year"><?php echo date("Y", $node->created); ?></span>
+        </div>
+
+        <h4 class="h4 subsite-spot-heading-title">
+            <?php print $title; ?>
+        </h4>
+
+        <?php if (isset($content['body'])): ?>
+            <span class="subsite-teaser-body-content">
+                <?php print render($content['body']); ?>
+            </span>
+        <?php endif; ?>
+
+    </a>
 </article>
-
-
