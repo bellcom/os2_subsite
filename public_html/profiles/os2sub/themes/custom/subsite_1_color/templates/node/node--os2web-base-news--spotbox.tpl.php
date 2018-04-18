cb58e7493629b03ee271051b45a2b3c4599216ca
Franz Skaaning <fs@bellcom.dk>
2018-04-18 08:52:01 +0200

    Spotbox version of news, transl month

--- public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/node/node--os2web-base-news--spotbox.tpl.php
+++ public_html/profiles/os2sub/themes/custom/subsite_1_color/templates/node/node--os2web-base-news--spotbox.tpl.php
@@ -12,7 +12,7 @@
     <a class="news-spot-link" href="<?php print $node_url; ?>">
         <div class="date-in-parts">
             <span class="day"><?php echo date("j", $node->created); ?>. </span>
-            <span class="month"><?php echo date("F", $node->created); ?></span>
+            <span class="month"><?php echo t(date("F", $node->created)); ?></span>
             <span class="year"><?php echo date("Y", $node->created); ?></span>
         </div>
 
